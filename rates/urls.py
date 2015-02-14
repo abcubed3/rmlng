71355e8eaaabcac83336e8534083a51539eafeee
Abdul <abdul@abdul-Ubuntu.(none)>
2015-02-14 23:54:35 +0100

    user adding ratings

--- rates/urls.py
+++ rates/urls.py
@@ -4,7 +4,8 @@ from rates import views
 
 urlpatterns = patterns('',
 #    url(r'^$', views.index, name='index'),
-    url(r'^$', views.rating, name='rating'),
+    url(r'^$', views.rating, name='allRatings'),
     url(r'^/(?P<slug>[\w-]+)/$', views.ratelecturer, name='ratelecturer'),
+    url(r'^/(?P<slug>[\w-]+)/add$', views.addRating, name='addRating'),
     #url(r'^/(?P<id>[\w-]+)/get_help/$', views.get_help, name='get_help'),
 )
--- rates/views.py
+++ rates/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
 # Create your views here.
 from .models import Rating
+from .forms import RatingForm
 from lecturers.models import Lecturer
 
 def rating(request):
@@ -18,11 +20,39 @@ def rating(request):
 #    template = 'rates/rating.html'
 #    return render(request, template, context)
    
-
-
+def addRating(request, slug):
+    try:
+        lect = Lecturer.objects.get(slug=slug)
+    except Lecturer.DoesNotExist:
+        pass
+    except:
+        pass
+    rate = Rating(rating =4.5, lecturer= lect, helpfulness="3", knowlegdeable="3", clarity="3", hotness="2", handout="2", course="EEG505", recommend="4", describe = "Description")
+    rate.save()
+    return HttpResponseRedirect(reverse('rates:allRatings'))
+    
+#def addRating(request, slug):
+#    #try:
+#    #    lect = Lecturer.objects.get(slug=slug)
+#    #except Lecturer.DoesNotExist:
+#    #    pass
+#    #except:
+#    #    pass
+#    form = RatingForm(request.POST or None)
+#    if form.is_valid():
+#        save_it = form.save(commit=False)
+#        save_it.save()
+#        
+#    return HttpResponseRedirect(reverse("ratelecturer"))
+    
 def ratelecturer(request, slug):
     slug = slug
-    lect = Lecturer.objects.get(slug=slug)
+    try:
+        lect = Lecturer.objects.get(slug=slug)
+    except Lecturer.DoesNotExist:
+        pass
+    except:
+        pass
     rates = Rating.objects.all().filter(lecturer=lect)
     counter = Rating.objects.filter(lecturer=lect).filter(helpfulness=5).count()
     context =  {'rates': rates, 'counter':counter}
